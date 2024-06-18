80f7a500c00f3d774162101f61fba48f48b5860b
Mwiinga Cheelo <30971762+mwiingac@users.noreply.github.com>
2024-06-18 20:39:55 +0200

    "Updated index.html to rename 'Research Data Orientation' to 'RaDO' and modified scripts.js to add new nodes and update node positions."

--- scripts.js
+++ scripts.js
@@ -2,8 +2,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const mindmapContainer = document.getElementById('mindmap-container');
 
     const nodes = [
-        { id: 'data-node', text: 'Data', x: '40%', y: '60%' },
-        { id: 'research-node', text: 'Research', x: '60%', y: '60%'},
+        
+        { id: 'research-node', text: 'Research',            x: '45%', y: '50%'},
+        { id: 'project-open', text: 'Project Open',         x: '25%', y: '15%' },
+        { id: 'survey-development', text: 'Survey Dev.',    x: '25%', y: '30%' },
+        { id: 'survey-preparation', text: 'Survey Prep.',   x: '25%', y: '45%' },
+        { id: 'survey', text: 'Survey',                     x: '25%', y: '60%' },
+        { id: 'post-survey', text: 'Post Survey',           x: '25%', y: '75%' },
+        { id: 'project-close', text: 'Project Close',       x: '25%', y: '90%' },
+
+        { id: 'data-node', text: 'Data',                    x: '55%', y: '50%' },
+        { id: 'data-collection', text: 'Collection',        x: '70%', y: '15%' },
+        { id: 'data-management', text: 'Management',        x: '70%', y: '30%' },
+        { id: 'data-cleaning', text: 'Clenaing',            x: '70%', y: '45%' },
+        { id: 'data-sharing', text: 'Sharing',              x: '70%', y: '60%' },
+        { id: 'data-analysis', text: 'Analysis',            x: '70%', y: '75%' },
+        { id: 'data-reporting', text: 'Reporting',          x: '70%', y: '90%' }
+
+        /*
         { id: 'po-establish-norms', text: 'Establish working norms', x: '30%', y: '70%' },
         { id: 'po-doc-research-design', text: 'Document research design', x: '30%', y: '70%' },
         { id: 'po-budget', text: 'Budget and staffing', x: '30%', y: '70%' },
@@ -17,6 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
         { id: 'po-salesforce', text: 'Update Salesforce records', x: '30%', y: '70%' },
         { id: 'po-box-save', text: 'Save all files on Box', x: '30%', y: '70%' },
         { id: 'po-pii-vault', text: 'Save all PII files in Cryptomator Vaults', x: '30%', y: '70%' }
+        */
         // Add other nodes here
         // { id: '', text: 'Case 104570 - Investigate adding Matomo to Base Platform', x: '30%', y: '70%' }
         // More nodes...
@@ -30,7 +47,6 @@ document.addEventListener("DOMContentLoaded", () => {
         newNode.style.top = node.y;
         newNode.textContent = node.text;
         mindmapContainer.appendChild(newNode);
-
         createLine('center-node', node.id);
     });
 });
