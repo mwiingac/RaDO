ee323bf6cb4063d13ec40d3b1cdce45c04660cd4
Mwiinga Cheelo <30971762+mwiingac@users.noreply.github.com>
2024-07-08 01:30:30 +0300

    # RaDO ready for phase one orientation

---
+++ scriptBlockScroll.js
@@ -0,0 +1,34 @@
+let currentPage = 1;
+const totalPages = 7;
+let isThrottled = false;
+
+function throttleScroll(func, limit = 1000) {
+    return (...args) => {
+        if (!isThrottled) {
+            func.apply(this, args);
+            isThrottled = true;
+            setTimeout(() => {
+                isThrottled = false;
+            }, limit);
+        }
+    };
+}
+
+function scrollPage(event) {
+    if (event.deltaY > 0) {
+        // Scroll Down
+        if (currentPage < totalPages) {
+            currentPage++;
+        }
+    } else {
+        // Scroll Up
+        if (currentPage > 1) {
+            currentPage--;
+        }
+    }
+    document.querySelectorAll('.page').forEach((page, index) => {
+        page.style.transform = `translateY(-${(currentPage - 1) * 100}vh)`;
+    });
+}
+
+window.addEventListener('wheel', throttleScroll(scrollPage));
